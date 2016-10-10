9cab78951a454fba774e7f82ef4f536e3f9865aa
saracubillas <sara.cubillas@gmail.com>
2016-10-10 11:01:16 +0200

    cleaning command

--- privacy/src/application/service/ViewChromosome.java
+++ privacy/src/application/service/ViewChromosome.java
@@ -4,25 +4,22 @@ public class ViewChromosome extends Command {
 
     @Override
     public void execute(String file) {
-        System.out.println("viewing chromosome ");
-
         String fileWithoutRef = file.substring(0, file.lastIndexOf('#'));
-        String fileName = fileWithoutRef.substring( fileWithoutRef.lastIndexOf('/')+1, fileWithoutRef.length() );//toy.sam
         String fileWithoutExtn = fileWithoutRef.substring(0, fileWithoutRef.lastIndexOf('.'));
-        String fileNameWithoutExtn = fileName.substring(0, fileName.lastIndexOf('.'));
         String chromosomeRef = file.substring(file.lastIndexOf("#") + 1);
 
         viewSAM(fileWithoutRef);
-        Command Index = new Index();
-        Index.execute(fileWithoutExtn + ".bam");
-/*
-        String command = "samtools view -h " + fileWithoutExtn + ".bam " + chromosomeRef + " > " +
-                fileWithoutExtn + "." + chromosomeRef + ".sam "; */
-
+        IndexBam(fileWithoutExtn);
         String command = "samtools view -h " + fileWithoutExtn + ".bam " + chromosomeRef;
+        
         String output = executeCommand(command);
         System.out.println(output);
         System.out.println("\n========OUTPUT======");
+    }
+
+    private void IndexBam(String fileWithoutExtn) {
+        Command Index = new Index();
+        Index.execute(fileWithoutExtn + ".bam");
     }
 
     private void viewSAM(String file) {
